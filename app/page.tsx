3bfd553e47ca5c28e7d9fee7204fa9d5588b24ca
Alejandro Alcocer Valencia <alejandro.alcocer-valencia@t-systems.com>
2025-11-29 15:36:17 +0700

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,31 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
+      <main className="min-h-screen flex flex-row items-center justify-between p-24">
+
+        <div className="">
+          <h1 className="text-4xl font-bold">Welcome to NordX</h1>
+          <p className="mt-4 text-lg text-gray-600">
+            Unlock Your Potential with Peptides
+          </p>
+        </div>
+
+        <div className="relative">
+          <Image
+            src="/nordx_3.png"
+            alt="NordX Peptides"
+            width={600}
+            height={400}
+            className="rounded-lg shadow-lg mt-8"
+          />
+          {/* Title inside the image */}
+          <div className="absolute top-10 left-4 p-4">
+            <h2 className="text-green-900/90 text-3xl font-bold px-4 py-2 rounded-lg">
+              NordX Peptides
+            </h2>
+          </div>
+        </div>
+      </main>
 
     </div>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,12 +5,10 @@ export const Footer = () => {
             <div className="h-26 ml-18 flex flex-col justify-around">
                 <p className="text-3xl">Follow us</p>
                 <div>
-                    <Image src="facebook.svg" alt="Facebook" width={24} height={24} className="inline-block mx-2" />
-                    <Image src="instagram.svg" alt="Instagram" width={24} height={24} className="inline-block mx-2" />
+                    <Image src="facebook_white.svg" alt="Facebook" width={24} height={24} className="inline-block mx-2" />
+                    <Image src="instagram_white.svg" alt="Instagram" width={24} height={24} className="inline-block mx-2" />
                 </div>
 
-                {/* <Image src="/icons/twitter.svg" alt="Twitter" width={24} height={24} className="inline-block mx-2"/> */}
-                {/* <Image src="/icons/instagram.svg" alt="Instagram" width={24} height={24} className="inline-block mx-2"/> */}
                 <p className="text-xs">
                     ©{new Date().getFullYear()} NordX. All rights reserved.
                 </p>
@@ -27,7 +25,7 @@ export const Footer = () => {
                     <p>WhatsApp</p>
                     <div className="flex flex-row">
                         <p>+66 992300072</p>
-                        <Image src="whatsapp.svg" alt="WhatsApp" width={24} height={24} className="inline-block mx-2" />
+                        <Image src="whatsapp_white.svg" alt="WhatsApp" width={24} height={24} className="inline-block mx-2" />
                     </div>
                 </div>
 
@@ -35,7 +33,7 @@ export const Footer = () => {
                     <p>Line</p>
                     <div className="flex flex-row">
                         <p>+66 992300072</p>
-                        <Image src="line.svg" alt="Line" width={24} height={24} className="inline-block mx-2" />
+                        <Image src="line_white.svg" alt="Line" width={24} height={24} className="inline-block mx-2" />
                     </div>
                 </div>
 
