8e1840ff518369779b4e05f36ebbccc5d0273e38
Alejandro Alcocer Valencia <alejandro.alcocer-valencia@t-systems.com>
2025-12-01 15:43:07 +0700

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -3,32 +3,25 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <main className="min-h-screen flex flex-row items-center justify-between p-24">
-
-        <div className="">
-          <h1 className="text-4xl font-bold">Welcome to NordX</h1>
-          <p className="mt-4 text-lg text-gray-600">
+      <main className="min-h-screen flex flex-col md:flex-row items-center justify-between p-6 md:p-24">
+        {/* Text Section */}
+        <div className="text-center md:text-left mb-8 md:mb-0">
+          <h1 className="text-3xl md:text-4xl font-bold">Welcome to NordX</h1>
+          <p className="mt-4 text-base md:text-lg text-gray-600">
             Unlock Your Potential with Peptides
           </p>
         </div>
 
-        <div className="relative">
+        <div className="relative w-full md:w-auto">
           <Image
             src="/nordx_3.png"
             alt="NordX Peptides"
             width={600}
             height={400}
-            className="rounded-lg shadow-lg mt-8"
+            className="rounded-lg shadow-lg mt-8 mx-auto md:mx-0"
           />
-          {/* Title inside the image */}
-          <div className="absolute top-10 left-4 p-4">
-            <h2 className="text-green-900/90 text-3xl font-bold px-4 py-2 rounded-lg">
-              NordX Peptides
-            </h2>
-          </div>
         </div>
       </main>
-
     </div>
   );
 }
